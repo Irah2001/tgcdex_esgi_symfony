01905b1a5642641e2a004567b9b6ba85bef5a8fd
LinuxCat <masch77.linuxcat@gmail.com>
2025-02-05 12:52:57 +0100

    Fix : fix crashes in kernel

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use App\Entity\PokemonCard;
+use Exception;
 use Pokedex;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Console\Helper\ProgressBar;
@@ -16,44 +17,49 @@ class Kernel extends BaseKernel
     function boot(): void
     {
         parent::boot();
-        set_time_limit(300);
-        ini_set('memory_limit',' 2048M');
-        
-        $output = new ConsoleOutput();
-
-        $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
-        $cr = $entityManager->getRepository(PokemonCard::class);
-        if (count($cr->findAll()) != 0){
-            $output->write("Database not empty! Skipping.");
-        } else {
-
-            $pkdex = new Pokedex();
-            $tgcDex = $pkdex->getDex("fr");
-    
-            $sets = $tgcDex->set->list();
-            if (count($sets) == 0) {
-                $output->write("No sets available - is the API online ?");
-            } 
-    
-            $amount = 0;
+        try{
+            set_time_limit(300);
+            ini_set('memory_limit',' 2048M');
             
-            $setBar = new ProgressBar($output, count($sets));
-            $setBar->start();
-            foreach ($sets as $j => $set) {
-                $cardSet = $tgcDex->set->get($set->id);
-                foreach ($cardSet->cards as $i => $card) {
-                    $c = new PokemonCard();
-                    if (empty($card->image)) continue;
+            $output = new ConsoleOutput();
+            $output->writeln($this->environment);
+
+            $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+            $cr = $entityManager->getRepository(PokemonCard::class);
+            if (count($cr->findAll()) != 0){
+                $output->write("Database not empty! Skipping.");
+            } else {
+
+                $pkdex = new Pokedex();
+                $tgcDex = $pkdex->getDex("fr");
+            
+                $sets = $tgcDex->set->list();
+                if (count($sets) == 0) {
+                    $output->write("No sets available - is the API online ?");
+                } 
+            
+                $amount = 0;
+
+                $setBar = new ProgressBar($output, count($sets));
+                $setBar->start();
+                foreach ($sets as $j => $set) {
+                    $cardSet = $tgcDex->set->get($set->id);
+                    foreach ($cardSet->cards as $i => $card) {
+                        $c = new PokemonCard();
+                        if (empty($card->image)) continue;
+
+                        $c->setImgUrl($card->image."/high.png");
+                        $c->setName($card->name);
                     
-                    $c->setImgUrl($card->image."/high.png");
-                    $c->setName($card->name);
-    
-                    $cr->save($c, $j == array_key_last($sets) && $i == array_key_last($cardSet->cards));
-                    $amount++;
+                        $cr->save($c, $j == array_key_last($sets) && $i == array_key_last($cardSet->cards));
+                        $amount++;
+                    }
+                    $setBar->advance();
                 }
-                $setBar->advance();
+                $setBar->finish();
             }
-            $setBar->finish();
+        } catch (Exception) {
+            
         }
-    }
+    }   
 }
