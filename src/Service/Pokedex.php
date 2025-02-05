02b929b5d8b1407e956d1c5268e101ac06db0d52
LinuxCat <masch77.linuxcat@gmail.com>
2025-02-05 11:11:18 +0100

    chore: populate database on boot (#1)

---
+++ src/Service/Pokedex.php
@@ -0,0 +1,25 @@
+<?php
+
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use TCGdex\TCGdex;
+use Symfony\Component\HttpClient\Psr18Client;
+
+/**
+ * Used to get the pokedex to do requests
+ */
+class Pokedex {
+    /**
+     * Get TCG Dex using language
+     */
+    public function getDex($lang = "fr") : TCGdex {
+        TCGdex::$requestFactory = new Psr17Factory();
+        TCGdex::$responseFactory = new Psr17Factory();
+
+        TCGdex::$client = new Psr18Client();
+
+        return new TCGdex($lang);
+    }
+}
+?>
