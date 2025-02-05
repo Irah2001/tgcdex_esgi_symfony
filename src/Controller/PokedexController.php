0e1e652ff9d2aaf787c7ee7a1b5c094161fb124b
Irina RASAMIARIMANANA <irinarasams1@gmail.com>
2025-02-05 12:11:08 +0100

    Consult pokedex (cards owned / cards not owned)

---
+++ src/Controller/PokedexController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\PokemonCardRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+final class PokedexController extends AbstractController
+{
+    #[Route('/pokedex', name: 'app_pokedex')]
+    #[IsGranted('ROLE_USER')]
+    public function index(PokemonCardRepository $pokemonCardRepository): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            throw $this->createNotFoundException("Utilisateur non authentifié.");
+        }
+
+        $ownedCards = $user->getPokedex();
+        $notOwnedCards = $pokemonCardRepository->findNotOwnedByUser($user->getId());
+
+        return $this->render('pokedex/index.html.twig', [
+            'ownedCards' => $ownedCards,
+            'notOwnedCards' => $notOwnedCards,
+        ]);
+    }
+}
